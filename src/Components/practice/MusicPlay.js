c0921e0b80cd5656b9d2dd11e68580b8c96b9b7a
neetuy <neetuy1455@gmail.com>
2017-07-28 15:07:42 +0530

    on playing song it shows decoding error

--- src/Components/practice/MusicPlay.js
+++ src/Components/practice/MusicPlay.js
@@ -1,187 +1,48 @@
 import React, {Component} from 'react';
+import { TouchableOpacity, Text, View, StyleSheet} from 'react-native';
 
-import {StyleSheet, Text, TouchableOpacity, View, ScrollView, Alert} from 'react-native';
+import {Icon} from 'native-base';
 import Sound from 'react-native-sound';
+
+export default class MusicPlay extends Component {
+ playSound = () =>{
+    
+   var whoosh = new Sound('frog.wav', Sound.MAIN_BUNDLE, (error) => {
+  if (error) {
+    console.log('failed to load the sound', error);
+    return;
+  } 
+  // loaded successfully
+  console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+});
+
+whoosh.play((success) => {
+  if (success) {
+    console.log('successfully finished playing');
+  } else {
+    console.log('playback failed due to audio decoding errors');
+  }
+});
+  }
+
+  render() {
+
+    return (
+      <View style ={styles.container}>
+    <TouchableOpacity onPress={this.playSound} >
+      <Icon name='ios-play' size={22} color={'rgb(44, 157, 51)'} />
+      <Text>Play Call</Text>
+      </TouchableOpacity>
+  </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  scrollContainer: {},
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    paddingTop: 30,
-    padding: 20,
-    textAlign: 'center',
-    backgroundColor: 'rgba(240,240,240,1)',
-  },
-  button: {
-    fontSize: 20,
-    backgroundColor: 'rgba(220,220,220,1)',
-    borderRadius: 4,
-    borderWidth: 1,
-    borderColor: 'rgba(80,80,80,0.5)',
-    overflow: 'hidden',
-    padding: 7,
-  },
-  header: {
-    textAlign: 'left',
-  },
-  feature: {
-    flexDirection: 'row',
-    padding: 10,
-    alignSelf: 'stretch',
+    justifyContent: 'center',
     alignItems: 'center',
-    borderTopWidth: 1,
-    borderTopColor: 'rgb(180,180,180)',
-    borderBottomWidth: 1,
-    borderBottomColor: 'rgb(230,230,230)',
+    backgroundColor: '#F5FCFF',
   },
-});
-
-const Button = ({title, onPress}) => (
-  <TouchableOpacity onPress={onPress}>
-    <Text style={styles.button}>{title}</Text>
-  </TouchableOpacity>
-);
-
-const Header = ({children, style}) => <Text style={[styles.header, style]}>{children}</Text>;
-
-const Feature = ({title, onPress, description, buttonLabel = 'PLAY', status}) => (
-  <View style={styles.feature}>
-    <Header style={{flex: 1}}>{title}</Header>
-    {status ? <Text style={{padding: 5}}>{resultIcons[status] || ''}</Text> : null}
-    <Button title={buttonLabel} onPress={onPress} />
-  </View>
-);
-
-const resultIcons = {
-  '': '',
-  pending: '?',
-  playing: '\u25B6',
-  win: '\u2713',
-  fail: '\u274C',
-};
-
-const audioTests = [
-  {
-    title: 'mp3 in bundle',
-    url: 'advertising.mp3',
-    basePath: Sound.MAIN_BUNDLE,
-  },
-  {
-    title: 'mp3 in bundle (looped)',
-    url: 'advertising.mp3',
-    basePath: Sound.MAIN_BUNDLE,
-    onPrepared: (sound, component) => {
-      sound.setNumberOfLoops(-1);
-      component.setState({loopingSound: sound});
-    },
-  },
-  {
-    title: 'mp3 remote download',
-    url: 'https://raw.githubusercontent.com/zmxv/react-native-sound-demo/master/advertising.mp3',
-  },
-  {
-    title: 'mp3 remote - file doesn\'t exist',
-    url: 'https://raw.githubusercontent.com/zmxv/react-native-sound-demo/master/file-not-here.mp3',
-  },
-  {
-    title: 'aac remote download',
-    url: 'https://raw.githubusercontent.com/zmxv/react-native-sound-demo/master/pew2.aac',
-  },
-  {
-    title: 'wav remote download',
-    url: 'https://raw.githubusercontent.com/zmxv/react-native-sound-demo/master/frog.wav',
-  },
-  
-];
-
-function setTestState(testInfo, component, status) {
-  component.setState({tests: {...component.state.tests, [testInfo.title]: status}});
-}
-
-/**
- * Generic play function for majority of tests
- */
-function playSound(testInfo, component) {
-  setTestState(testInfo, component, 'pending');
-
-  const callback = (error, sound) => {
-    if (error) {
-      Alert.alert('error', error.message);
-      setTestState(testInfo, component, 'fail');
-      return;
-    }
-    setTestState(testInfo, component, 'playing');
-    // Run optional pre-play callback
-    testInfo.onPrepared && testInfo.onPrepared(sound, component);
-    sound.play(() => {
-      // Success counts as getting to the end
-      setTestState(testInfo, component, 'win');
-      // Release when it's done so we're not using up resources
-      sound.release();
-    });
-  };
-
-  // If the audio is a 'require' then the second parameter must be the callback.
-  if (testInfo.isRequire) {
-    const sound = new Sound(testInfo.url, error => callback(error, sound));
-  } else {
-    const sound = new Sound(testInfo.url, testInfo.basePath, error => callback(error, sound));
-  }
-}
-
-class MainView extends Component {
-  constructor(props) {
-    super(props);
-
-    Sound.setCategory('Playback', true); // true = mixWithOthers
-
-    // Special case for stopping
-    this.stopSoundLooped = () => {
-      if (!this.state.loopingSound) {
-        return;
-      }
-
-      this.state.loopingSound.stop().release();
-      this.setState({loopingSound: null, tests: {...this.state.tests, ['mp3 in bundle (looped)']: 'win'}});
-    };
-
-    this.state = {
-      loopingSound: undefined,
-      tests: {},
-    };
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Header style={styles.title}>react-native-sound-demo</Header>
-        <ScrollView style={styles.container} contentContainerStyle={styles.scrollContainer}>
-          {audioTests.map(testInfo => {
-            return (
-              <Feature
-                status={this.state.tests[testInfo.title]}
-                key={testInfo.title}
-                title={testInfo.title}
-                onPress={() => {
-                  return playSound(testInfo, this);
-                }}
-              />
-            );
-          })}
-          <Feature title="mp3 in bundle (looped)" buttonLabel={'STOP'} onPress={this.stopSoundLooped} />
-        </ScrollView>
-      </View>
-    );
-  }
-}
-
-export default MainView;
-
-
-
-
-
+})
