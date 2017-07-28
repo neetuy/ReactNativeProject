09cc0d9c522320d5a785786a450b09a21d9b1266
neetuy <neetuy1455@gmail.com>
2017-07-28 16:55:48 +0530

    play music done

--- src/Components/practice/MusicPlay.js
+++ src/Components/practice/MusicPlay.js
@@ -5,25 +5,41 @@ import {Icon} from 'native-base';
 import Sound from 'react-native-sound';
 
 export default class MusicPlay extends Component {
- playSound = () =>{
-    
-   var whoosh = new Sound('frog.wav', Sound.MAIN_BUNDLE, (error) => {
-  if (error) {
-    console.log('failed to load the sound', error);
-    return;
-  } 
-  // loaded successfully
-  console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
-});
+playSound() {
+      // let s = new Sound('frog.wav','', (e) => {
+      //   if (e) {
+      //     console.log('error', e);
+      //   } else {
+      //     console.log('duration', s.getDuration());
+      //     s.play();
+      //   }
+      // });
+         var whoosh = new Sound('frog.wav', Sound.MAIN_BUNDLE, (error) => {
+            if (error) {
+              console.log('failed to load the sound', error);
+            } 
+            // loaded successfully
+            
+            console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+              whoosh.play(()=>{
+                whoosh.release()
+            });
+          });
 
-whoosh.play((success) => {
-  if (success) {
-    console.log('successfully finished playing');
-  } else {
-    console.log('playback failed due to audio decoding errors');
+          whoosh.play((success) => {
+            if (success) {
+              console.log('successfully finished playing');
+            } else {
+              console.log('playback failed due to audio decoding errors');
+            }
+        });
+          
   }
-});
+
+componenWillMount(){
+    setTimeout(this.playSound(), 5000);
   }
+
 
   render() {
 
