2c443fe23dc8f7ec8c1c92a6d79e7694c882266b
neetuy <neetuy1455@gmail.com>
2017-07-31 17:06:12 +0530

    react-native-maps

--- src/Components/practice/GoogleMapsExample.js
+++ src/Components/practice/GoogleMapsExample.js
@@ -1,43 +1,47 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
+import{
   StyleSheet,
-  Text,
-  View
 } from 'react-native';
 import MapView from 'react-native-maps';
 
-export default class MyApp extends Component {
-  render() {
-    const { region } = this.props;
-    console.log(region);
-
-    return (
-      <View style ={styles.container}>
-        <MapView
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        >
-        </MapView>
-      </View>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
+
+export default class GoogleMapsExample extends Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <MapView
+        style={ styles.map }
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    );
+  }
+
+}
+
