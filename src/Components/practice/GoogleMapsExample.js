6c26d064b4f4998661f829f80ac7ee9fe7e9e6ea
neetuy <neetuy1455@gmail.com>
2017-08-01 11:56:14 +0530

    map-marker

--- src/Components/practice/GoogleMapsExample.js
+++ src/Components/practice/GoogleMapsExample.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import{
+import {
   StyleSheet,
+  Text,
+  View,
+  Image,
+  ListView,
+  TextInput,
+  TouchableHighlight,
+  Dimensions,
+ //MapView,
 } from 'react-native';
+
 import MapView from 'react-native-maps';
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
   map: {
     position: 'absolute',
     top: 0,
@@ -21,27 +21,64 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
   },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 30,
+    flex: 1,
+    alignItems: 'center'
+  },
+  inputText: {
+    height: 36,
+      padding: 4,
+      marginRight: 5,
+      flex: 4,
+      fontSize: 18,
+      borderWidth: 1,
+      borderColor: '#48BBEC',
+      borderRadius: 8,
+      color: '#48BBEC'
+  }
 });
 
-export default class GoogleMapsExample extends Component {
+class MapPage extends Component{
 
-  constructor() {
-    super();
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            region:{
+                latitude: 45.65,
+                longitude: -78.90,
+            latitudeDelta: 10,
+            longitudeDelta: 5
+            }
+        }
+    }
 
-  render() {
-    return (
-      <MapView
-        style={ styles.map }
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    );
-  }
+    render() {
 
-}
+        return(
+            <View style={styles.container}>
+                <TextInput style={styles.inputText}></TextInput>
+                <MapView 
+                    style={ styles.map }
+                    mapType={"standard"}
+                    region={this.state.region}
+                    zoomEnabled={true}
+                    scrollEnabled={true}
+                    showsScale={true}
+                  >
+                  <MapView.Marker
+                    coordinate={{latitude: 45.65,
+                    longitude: -78.90}}
+                    title={"title"}
+                    description={"description"}
+                 />
+                  </MapView>
+            </View>
+            )
+      }
+    }
 
+
+module.exports = MapPage;
