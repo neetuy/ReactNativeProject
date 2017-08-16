c7564fd961be37c2d3fa803456630686de470e92
neetuy <neetuy1455@gmail.com>
2017-08-16 17:14:06 +0530

    fb integration with app

--- src/Components/practice/FBLinkingExample.js
+++ src/Components/practice/FBLinkingExample.js
@@ -1,6 +1,6 @@
 
-// import React, { Component } from 'react'
-// import { View, StyleSheet, Animated, TouchableOpacity } from 'react-native'
+import React, { Component } from 'react'
+import { View, StyleSheet, Animated, TouchableOpacity } from 'react-native'
 import SplashScreen from 'react-native-splash-screen'
 // import  {LoginButton,AccessToken} from 'react-native-fbsdk';
 
@@ -38,52 +38,64 @@ import SplashScreen from 'react-native-splash-screen'
 // };
 
 
-import React, { Component, PropTypes } from 'react';
-import { LoginButton, AccessToken } from 'react-native-fbsdk';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity
-} from 'react-native';
+// import React, { Component, PropTypes } from 'react';
+// import { LoginButton, AccessToken } from 'react-native-fbsdk';
+// import {
+//   StyleSheet,
+//   Text,
+//   View,
+//   TouchableOpacity
+// } from 'react-native';
 
-class FbLoginButton extends Component {
+// class FbLoginButton extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+//   constructor(props) {
+//     super(props);
+//   }
 
-  componentDidMount() {
-    SplashScreen.hide();
-  }
+//   componentDidMount() {
+//     SplashScreen.hide();
+//   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-      <LoginButton
-        publishPermissions={["publish_actions"]}
-        onLoginFinished={
-          (error, result) => {
-            if (error) {
-              alert("login has error: " + result.error);
-              console.log(result.error)
-            } else if (result.isCancelled) {
-              alert("login is cancelled.");
-            } else {
-              AccessToken.getCurrentAccessToken().then(
-                (data) => {
-                  alert(data.accessToken.toString())
-                }
-              )
-            }
-          }
-        }
-        onLogoutFinished={() => alert("logout.")}/>
-      </View>
-    );
-  }
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//       <LoginButton
+//          onLoginFinished={
+ 
+//               (error, result) => {
+ 
+//               if (error) {
+ 
+//                 console.log("login has error: " + result.error);
+ 
+//               } else if (result.isCancelled) {
+ 
+//                 console.log("login is cancelled.");
+ 
+//               } else {
+ 
+//                 AccessToken.getCurrentAccessToken().then(
+ 
+//                   (data) => {
+ 
+//                     console.log(data);
+ 
+//                   }
+ 
+//                 )
+ 
+//               }
+ 
+//             }
+ 
+//           }
+ 
+//           onLogoutFinished={() => console.log("logout.")}/>
+//     );
+//   }
 
-}
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -104,4 +116,32 @@ const styles = StyleSheet.create({
   },
 });
 
-export default FbLoginButton;
+
+import FBSDK, { LoginButton } from 'react-native-fbsdk';
+
+const onLoginFinished = (error, result) => {
+  if (error) {
+    alert("login has error: " + error);
+    console.log( result.error)
+  } else if (result.isCancelled) {
+    alert("login is cancelled.");
+  } else {
+    alert("login has finished with permissions: " + result.grantedPermissions)
+  }
+};
+
+export default class FbLoginButton extends Component {
+  componentDidMount() {
+      SplashScreen.hide();
+    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={onLoginFinished}
+          onLogoutFinished={() => alert("logout.")}/>
+      </View>
+    );
+  }
+}
